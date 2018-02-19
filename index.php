9752194cd5d3d50102da924b83dff351b84ab150
agentcobra <agentcobra@free.fr>
2018-02-19 19:13:08 +0100

    Fix copyright and other stuff

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php
-// Copyright (C) 2004-2006 Jarno Elonen <elonen@iki.fi>
-//
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-// * Redistributions of source code must retain the above copyright notice, this
-//   list of conditions and the following disclaimer.
-// * Redistributions in binary form must reproduce the above copyright notice,
-//   this list of conditions and the following disclaimer in the documentation
-//   and/or other materials provided with the distribution.
-// * The name of the author may not be used to endorse or promote products derived
-//   from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR
-// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/**
+ * Copyright (C) 2004-2006 Jarno Elonen <elonen@iki.fi>
+ * Copyright (C) 2015-2018 Agentcobra <agentcobra@free.fr>
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
 require_once("config.inc.php");
 require_once("utils.inc.php");
@@ -30,133 +34,124 @@ forceNoCache();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Translate '<?php print_encoded($project_name);  ?>' - PHPPOEdit</title>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap -->
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Translate '<?php print_encoded($project_name); ?>' - PHPPOEdit</title>
+</head>
 
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+<body>
 
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
-        <style>
-            footer.well{
-                margin-bottom: 0;
-            }
-        </style>
-    </head>
+<h1><?php print_encoded($project_name); ?> translations</h1>
 
-    <body>
-        <h1><?php print_encoded($project_name);  ?> translations</h1>
+<div class="alert alert-info" role="alert">
+    NOTE! This system <em>requires</em> UTF-8 character encoding.
+    If you don't see some Japanese text (or plain squares) here: "日本語",
+    adjust your browser settings.
+</div>
+<?php
 
-        <div class="alert alert-info" role="alert">
-            NOTE! This system <em>requires</em> UTF-8 character encoding.
-            If you don't see some Japanese text (or plain squares) here: "日本語",
-            adjust your browser settings.
-        </div>
-        <?php
-
-$pot_files = array_map(
-    create_function('$t', 'return str_replace("pot/", "", $t);'),
-    my_glob( "pot/*.pot" ));
+$pot_files = array_map(function ($t) {
+    return str_replace("pot/", "", $t);
+},
+    my_glob("pot/*.pot"));
 
 $lang_dirs = array_map(
-    create_function('$t', 'return str_replace("po/", "", $t);'),
-    my_glob( "po/*", GLOB_ONLYDIR ));
-        ?>
-        <h2>Languages:</h2>
-        <ul class="list-inline">
-            <?php
-foreach( $lang_dirs as $l )
-{
-    printf ("<li><a href='#%s'>%s</a></li>\n", xhtml_encode($l), xhtml_encode(ucfirst($l)));
-}
-            ?>
-        </ul>
-        <div class="container">
-            <?php
-foreach( $lang_dirs as $l )
-{
-    print '<h3 id="'. htmlentities($l) .'">' . xhtml_encode(ucfirst($l)) . '</h3>';
+    function ($t) {
+        return str_replace("po/", "", $t);
+    },
+    my_glob("po/*", GLOB_ONLYDIR));
+?>
+<h2>Languages:</h2>
+<ul class="nav justify-content-center">
+    <?php foreach ($lang_dirs as $l) : ?>
+        <li class="nav-item">
+            <?php printf("<a class='nav-link' href='#%s'>%s</a>\n", xhtml_encode($l), xhtml_encode(ucfirst($l))); ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+<div class="container">
+    <?php
+    foreach ($lang_dirs
 
-            ?>
-            <table class="table table-bordered table-hover table-striped">
-               <thead>
-                <tr>
-                    <th>Filename</th>
-                    <th>Translated</th>
-                    <th>..of which fuzzy</th>
-                    <th>total strings</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php
-
-    foreach( $pot_files as $pot )
+    as $l)
     {
-        $parsed_pot = parse_po(file( "pot/$pot" ));
+    print '<h3 id="' . htmlentities($l) . '">' . xhtml_encode(ucfirst($l)) . '</h3>';
 
-        print '<tr>';
-        $po = "po/$l/" . str_replace('.pot', '.po', $pot);
-        if ( file_exists( $po ))
-        {
-            $parsed_po = parse_po(file( $po ));
-            $total = 0; $translated = 0; $fuzzy = 0;
-            foreach( $parsed_pot as $checksum => $entry )
-            {
-                if ( strlen($entry["msgid"] ))
-                {
-                    $total++;
-                    if ( isset($parsed_po[$checksum]))
-                    {
-                        if ( in_array('fuzzy', $parsed_po[$checksum]["flags"]))
-                        {
-                            $fuzzy++;
-                            $translated++;
+    ?>
+    <table class="table table-bordered table-hover table-striped">
+        <thead>
+        <tr>
+            <th><?php _("Filename"); ?></th>
+            <th>Translated</th>
+            <th>..of which fuzzy</th>
+            <th>total strings</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+
+        foreach ($pot_files as $pot) {
+            $parsed_pot = parse_po(file("pot/$pot"));
+
+            print '<tr>';
+            $po = "po/$l/" . str_replace('.pot', '.po', $pot);
+            if (file_exists($po)) {
+                $parsed_po = parse_po(file($po));
+                $total = 0;
+                $translated = 0;
+                $fuzzy = 0;
+                foreach ($parsed_pot as $checksum => $entry) {
+                    if (strlen($entry["msgid"])) {
+                        $total++;
+                        if (isset($parsed_po[$checksum])) {
+                            if (in_array('fuzzy', $parsed_po[$checksum]["flags"])) {
+                                $fuzzy++;
+                                $translated++;
+                            } else {
+                                if (strlen($parsed_po[$checksum]["msgstr"])) {
+                                    $translated++;
+                                }
+                            }
                         }
-                        else if ( strlen($parsed_po[$checksum]["msgstr"]))
-                            $translated++;
                     }
                 }
-            }
-            debug($pot);
-            printf ("<td><a href='editpo.php?file=%s'><em>%s</em></a></td><td style='color: #%s'>%0.2f%%</td><td style='color: #%s'>%0.2f%%</td><td>%d</td>",
+                debug($pot);
+                printf("<td><a href='editpo.php?file=%s'><em>%s</em></a></td><td style='color: #%s'>%0.2f%%</td><td style='color: #%s'>%0.2f%%</td><td>%d</td>",
                     htmlentities($po),
                     str_replace('.pot', '.po', $pot),
-                    blend_colors( "008000", "FF0000", ($total>0) ? ($translated/$total) : 1 ),
-                    ($total>0) ? ($translated*100/$total) : 100,
-                    blend_colors( "008000", "FF0000", ($translated>0) ? (1-$fuzzy/$translated) : 1 ),
-                    ($translated>0) ? ($fuzzy*100/$translated) : 0,
+                    blend_colors("008000", "FF0000", ($total > 0) ? ($translated / $total) : 1),
+                    ($total > 0) ? ($translated * 100 / $total) : 100,
+                    blend_colors("008000", "FF0000", ($translated > 0) ? (1 - $fuzzy / $translated) : 1),
+                    ($translated > 0) ? ($fuzzy * 100 / $translated) : 0,
                     $total);
+            } else {
+                print '<div class="alert alert-warning" role="alert">MISSING FILE ' . xhtml_encode($po) . '</div>';
+            }
+            print '</tr>';
         }
-        else
-        {
-            print '<div class="alert alert-warning" role="alert">MISSING FILE ' . xhtml_encode($po) . '</div>';
+        print "</tbody></table>\n";
         }
-        print '</tr>';
-    }
-    print "</tbody></table>\n";
-}
-                ?>
-                </div><!--/.container-->
-            <footer class="well">
-                <p>Powered by <a href="http://iki.fi/elonen/code/phppoedit/">Phppoedit</a>.</p>
-            </footer>
-
-            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-            <!-- Include all compiled plugins (below), or include individual files as needed -->
-            <!-- Latest compiled and minified JavaScript -->
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-            </body>
-        </html>
+        ?>
+</div><!--/.container-->
+<footer class="well">
+    <p>Powered by <a href="http://iki.fi/elonen/code/phppoedit/">Phppoedit</a>.</p>
+</footer>
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+</body>
+</html>
--- poparse.inc.php
+++ poparse.inc.php
@@ -1,181 +1,213 @@
 <?php
-// Copyright (C) 2004-2006 Jarno Elonen <elonen@iki.fi>
-//
-// Redistribution and use in source and binary forms, with or without modification,
-// are permitted provided that the following conditions are met:
-//
-// * Redistributions of source code must retain the above copyright notice, this
-//   list of conditions and the following disclaimer.
-// * Redistributions in binary form must reproduce the above copyright notice,
-//   this list of conditions and the following disclaimer in the documentation
-//   and/or other materials provided with the distribution.
-// * The name of the author may not be used to endorse or promote products derived
-//   from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
-// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
-// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR
-// BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/**
+ * Copyright (C) 2004-2006 Jarno Elonen <elonen@iki.fi>
+ * Copyright (C) 2015-2018 Agentcobra <agentcobra@free.fr>
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * - The name of the author may not be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
+
+/**
+ * @param $txt
+ * @return string
+ */
 function decode_po_quotes($txt)
 {
-  return stripcslashes(preg_replace('/"[^"]*$/', '', preg_replace('/^[^"]*"/', '', $txt)));
+    return stripcslashes(preg_replace('/"[^"]*$/', '', preg_replace('/^[^"]*"/', '', $txt)));
 }
 
-function parse_po( $lines )
+/**
+ * @param $lines
+ * @return array
+ */
+function parse_po($lines)
 {
-  $res = array();
-  $last_line_type = "comment";
+    $res = [];
+    $last_line_type = "comment";
 
-  $empty_entry = array(
-    "comments" => array(),
-    "autocomments" => array(),
-    "sources" => array(),
-    "flags" => array(),
-    "msgid" => "",
-    "msgstr" => "" );
-  $cur = $empty_entry;
+    $empty_entry = [
+        "comments" => [],
+        "autocomments" => [],
+        "sources" => [],
+        "flags" => [],
+        "msgid" => "",
+        "msgstr" => ""
+    ];
+    $cur = $empty_entry;
 
-  $lineno = 0;
-  foreach( $lines as $line )
-  {
-    $lineno++;
-    $line = trim($line);
+    $lineno = 0;
+    foreach ($lines as $line) {
+        $lineno++;
+        $line = trim($line);
 
-    if (strlen($line))
-    {
-      if ( preg_match('/^#/', $line))
-      {
-        if ( $last_line_type != "comment" )
-        {
-          if ( strlen($cur["msgid"]) || strlen(join($cur["comments"])))
-            $res[] = $cur;
-          $cur = $empty_entry;
+        if (strlen($line)) {
+            if (preg_match('/^#/', $line)) {
+                if ($last_line_type != "comment") {
+                    if (strlen($cur["msgid"]) || strlen(join($cur["comments"]))) {
+                        $res[] = $cur;
+                    }
+                    $cur = $empty_entry;
+                }
+
+                if (preg_match('/^#:/', $line)) {
+                    $cur["sources"][] = trim(substr($line, 2));
+                } else {
+                    if (preg_match('/^#,/', $line)) {
+                        $cur["flags"] = preg_split('/[#, \t]+/', $line, -1, PREG_SPLIT_NO_EMPTY);
+                    } else {
+                        if (preg_match('/^#[.]/', $line)) {
+                            $cur["autocomments"][] = trim(substr($line, 2));
+                        } else {
+                            $cur["comments"][] = trim(substr($line, 2));
+                        }
+                    }
+                }
+
+                $last_line_type = "comment";
+            } else {
+                if (preg_match('/^msgid[ \t]+["]*/', $line)) {
+                    if ($last_line_type != "comment") {
+                        if (strlen($cur["msgid"]) || strlen($cur["comments"])) {
+                            $res[] = $cur;
+                        }
+                        $cur = $empty_entry;
+                    }
+                    $cur["msgid"] = decode_po_quotes(substr($line, 6));
+                    $last_line_type = "msgid";
+                } else {
+                    if (preg_match('/^msgstr[ \t]+["]*/', $line)) {
+                        if ($cur === false) {
+                            $cur = $empty_entry;
+                        }
+
+                        $cur["msgstr"] = decode_po_quotes($line);
+                        $last_line_type = "msgstr";
+                    } else {
+                        if (preg_match('/^"/', $line)) {
+                            if (!preg_match('/msg(id|str)/', $last_line_type)) {
+                                print "Warning: syntax error in PO-file on line $lineno \n";
+                            } else {
+                                $cur[$last_line_type] .= decode_po_quotes($line);
+                            }
+                        } else {
+                            print "Warning: malformed line $lineno in PO-file.\n";
+                        }
+                    }
+                }
+            }
         }
-
-        if ( preg_match('/^#:/', $line))
-          $cur["sources"][] = trim(substr($line, 2));
-        else if ( preg_match('/^#,/', $line))
-          $cur["flags"] = preg_split('/[#, \t]+/', $line, -1, PREG_SPLIT_NO_EMPTY);
-        else if ( preg_match('/^#[.]/', $line))
-          $cur["autocomments"][] = trim(substr($line, 2));
-        else
-          $cur["comments"][] = trim(substr($line, 2));
-
-        $last_line_type = "comment";
-      }
-      else if ( preg_match('/^msgid[ \t]+["]*/', $line))
-      {
-        if ( $last_line_type != "comment" )
-        {
-          if ( strlen($cur["msgid"]) || strlen($cur["comments"]) )
-            $res[] = $cur;
-          $cur = $empty_entry;
-        }
-        $cur["msgid"] = decode_po_quotes(substr($line, 6));
-        $last_line_type = "msgid";
-      }
-      else if ( preg_match('/^msgstr[ \t]+["]*/', $line))
-      {
-        if ( $cur === False )
-          $cur = $empty_entry;
-
-        $cur["msgstr"] = decode_po_quotes($line);
-        $last_line_type = "msgstr";
-      }
-      else if ( preg_match('/^"/', $line))
-      {
-        if ( !preg_match( '/msg(id|str)/', $last_line_type ))
-          print "Warning: syntax error in PO-file on line $lineno \n";
-        else
-          $cur[$last_line_type] .= decode_po_quotes($line);
-      }
-      else
-      {
-        print "Warning: malformed line $lineno in PO-file.\n";
-      }
     }
-  }
-  if ( strlen($cur["msgid"]) || strlen($cur["comments"]) )
-    $res[] = $cur;
+    if (strlen($cur["msgid"]) || strlen($cur["comments"])) {
+        $res[] = $cur;
+    }
 
-  $res2 = array();
-  foreach( $res as $r )
-    $res2[md5($r["msgid"])] = $r;
+    $res2 = array();
+    foreach ($res as $r) {
+        $res2[md5($r["msgid"])] = $r;
+    }
 
-  return $res2;
+    return $res2;
 }
 
-function slash_and_split_lines( $txt )
+/**
+ * @param $txt
+ * @return string
+ */
+function slash_and_split_lines($txt)
 {
     $res = "";
     // Split the string if it's very long
-    $broken_lines =  array();
-    while ( mb_strpos($txt, "\n", 0, 'UTF-8') !== False )
-    {
-      $i = mb_strpos($txt, "\n", 0, 'UTF-8');
-      $broken_lines[] = mb_substr($txt, 0, $i+1, 'UTF-8');
-      $txt = mb_substr($txt, $i+1, mb_strlen($txt, 'UTF-8'), 'UTF-8');
+    $broken_lines = array();
+    while (mb_strpos($txt, "\n", 0, 'UTF-8') !== false) {
+        $i = mb_strpos($txt, "\n", 0, 'UTF-8');
+        $broken_lines[] = mb_substr($txt, 0, $i + 1, 'UTF-8');
+        $txt = mb_substr($txt, $i + 1, mb_strlen($txt, 'UTF-8'), 'UTF-8');
     }
-    if (mb_strlen($txt, 'UTF-8'))
-      $broken_lines[] = $txt;
+    if (mb_strlen($txt, 'UTF-8')) {
+        $broken_lines[] = $txt;
+    }
 
-    foreach ( $broken_lines as $line )
-    {
-      $l = sprintf("\"%s\"\n", addcslashes($line,  "\"\0..\37\\"));
-      if ( mb_strlen($l, 'UTF-8') > 65 )
-      {
-        $l = "";
-        while ( mb_strlen($line, 'UTF-8') > 0 )
-        {
-            $l .= "\"" . addcslashes(mb_substr($line, 0, 65, 'UTF-8'),  "\"\0..\37\\") . "\"\n";
-            $line = mb_substr($line, 65, mb_strlen($line, 'UTF-8'), 'UTF-8');
+    foreach ($broken_lines as $line) {
+        $l = sprintf("\"%s\"\n", addcslashes($line, "\"\0..\37\\"));
+        if (mb_strlen($l, 'UTF-8') > 65) {
+            $l = "";
+            while (mb_strlen($line, 'UTF-8') > 0) {
+                $l .= "\"" . addcslashes(mb_substr($line, 0, 65, 'UTF-8'), "\"\0..\37\\") . "\"\n";
+                $line = mb_substr($line, 65, mb_strlen($line, 'UTF-8'), 'UTF-8');
+            }
         }
-      }
-      $res .= $l;
+        $res .= $l;
     }
 
-    if ( count($broken_lines) == 0 )
-      return "\"\"\n";
+    if (count($broken_lines) == 0) {
+        return "\"\"\n";
+    }
 
-    if ( substr_count($res, "\n") > 1 ) // mb_substr_count >= PHP 4.3
-      $res = "\"\"\n" . $res;
+    if (substr_count($res, "\n") > 1) // mb_substr_count >= PHP 4.3
+    {
+        $res = "\"\"\n" . $res;
+    }
 
     return $res;
 }
 
-function unparse_po( $entries )
+/**
+ * @param $entries
+ * @return string
+ */
+function unparse_po($entries)
 {
-  $res = "";
-  while ( list($k, $e) = each($entries))
-  {
-    foreach( $e["comments"] as $l )
-      if ( strlen($l))
-        $res .= "# " . $l . "\n";
+    $res = "";
+    while (list($k, $e) = each($entries)) {
+        foreach ($e["comments"] as $l) {
+            if (strlen($l)) {
+                $res .= "# " . $l . "\n";
+            }
+        }
 
-    foreach( $e["sources"] as $l )
-      if ( strlen($l))
-        $res .= "#: " . $l . "\n";
+        foreach ($e["sources"] as $l) {
+            if (strlen($l)) {
+                $res .= "#: " . $l . "\n";
+            }
+        }
 
-    foreach( $e["autocomments"] as $l )
-      if ( strlen($l))
-        $res .= "#. " . $l . "\n";
+        foreach ($e["autocomments"] as $l) {
+            if (strlen($l)) {
+                $res .= "#. " . $l . "\n";
+            }
+        }
 
-    if ( count($e["flags"]) > 0)
-      $res .= "#, " . join(", ", $e["flags"]) . "\n";
+        if (count($e["flags"]) > 0) {
+            $res .= "#, " . join(", ", $e["flags"]) . "\n";
+        }
 
-    $res .= sprintf( "msgid %s", slash_and_split_lines($e["msgid"]));
-    $res .= sprintf( "msgstr %s", slash_and_split_lines($e["msgstr"]));
+        $res .= sprintf("msgid %s", slash_and_split_lines($e["msgid"]));
+        $res .= sprintf("msgstr %s", slash_and_split_lines($e["msgstr"]));
 
-    $res .= "\n";
-  }
+        $res .= "\n";
+    }
 
-  return $res;
+    return $res;
 }
 
 ?>
